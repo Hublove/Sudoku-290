8a9dbc5c7d9e96f91e05a3a581f1c711d237b1f7
yaochen8 <popo.yao@mail.utoronto.ca>
2019-02-14 17:01:22 -0500

    add set stage

--- src/SudokuGame/View.java
+++ src/SudokuGame/View.java
@@ -1,9 +1,11 @@
 package SudokuGame;
 
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.animation.AnimationTimer;
 
 public class View extends Application{	
 	
@@ -16,8 +18,33 @@ public class View extends Application{
 		initUI(primaryStage);
 	}
 	
-	void initUI(Stage stage) {
-	
+	public Scene initStartScene() {
+		Scene scene = new Scene();
 	}
+	
+	public Scene initEndScene() {
+		Scene scene = new Scene();
+	}
+	
+	public Scene initGameScene() {
+		Scene scene = new Scene();
+	}
+	
+	void initUI(Stage stage) {
+		Scene scene = initStartScene();
+		Controller controller = new StartController();
+		
+		stage.setTitle("Sudoku game");
+		stage.setScene(scene);
+		stage.show();
+	}
+	
+	AnimationTimer timer = new AnimationTimer() {
+        @Override
+        public void handle(long now) {
+        	control.updateTimer();
+        }
+    };
+    timer.start();
 
 }
