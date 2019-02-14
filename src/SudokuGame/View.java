839af30f6e9b5f2a30e323f112482d8494bb0eba
yaochen8 <popo.yao@mail.utoronto.ca>
2019-02-14 16:52:55 -0500

    add main in view

--- src/SudokuGame/View.java
+++ src/SudokuGame/View.java
@@ -1,5 +1,23 @@
 package SudokuGame;
 
-public class View {
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class View extends Application{	
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		initUI(primaryStage);
+	}
+	
+	void initUI(Stage stage) {
+	
+	}
 
 }
