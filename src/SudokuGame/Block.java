336a3ee57c86a7b0516b2f63ad086efd1219ef48
Nabeel Rajabali <nabeelrajabali@gmail.com>
2019-03-20 12:21:58 -0400

    Updated and edited JavaDocs

--- src/SudokuGame/Block.java
+++ src/SudokuGame/Block.java
@@ -2,15 +2,16 @@ package SudokuGame;
 
 import java.util.ArrayList;
 /**
- *The class that will contain information about a single Block.
+ *The Block class will contain information about a single block from the board.
  */
 public class Block {
 
 	Box boxs[];
 	
 	/**
-	 *Initializes Block with have 9 empty Box classes.
+	 *Initializes Block with 9 empty Box classes.
 	 */
+	
 	public Block() {
 		this.boxs = new Box[9];
 		for (int i = 0; i < 9; i++) {
@@ -19,33 +20,33 @@ public class Block {
 	}
 	
 	/**
-	 * Returns the num of the Box at the given index. 
-	 * (count from left to right)
-	 * up to down
-	 * @param index index of the box
-	 * @return int the num value of the Box
+	 * Retrieves the number of the Box at the given index. 
+	 * Box index starts from left moving to right.
+	 * @param index index of the box.
+	 * @return the value the box holds.
 	 */
 	public int getNum(int index) {
 		return this.boxs[index].getNum();
 	}
 	
 	/**
-	 * Returns the num of the Box at the given xy-coordinate.
-	 * @param x x-coordinate
-	 * @param y y-coordinate
-	 * @return int the num value of the Box
+	 * Retrieves the value of the Box at the given xy-coordinate.
+	 * @param x x-coordinate.
+	 * @param y y-coordinate.
+	 * @return the integer value of the Box.
 	 */
+	
 	public int getNum(int x, int y) {
 		return getNum(x + y * 3);
 	}
 	
 	/**
-	 * Sets the num of the Box at the given index 
-	 * (count from left to right and up to down) 
-	 * and removes num from avalNum from all Boxes at this Block.
-	 * @param index index of the box
-	 * @param num the new num of that box
+	 * Allocates a number value to the Box at the given index.  
+	 * Removes number from avalNum from all Boxes on this Block.
+	 * @param index index of the box.
+	 * @param num the new number of the box.
 	 */
+	
 	public void setNum(int index, int num) {
 		this.boxs[index].setNum(num);
 		for (int i = 0; i < 9; i ++) {
@@ -54,99 +55,92 @@ public class Block {
 	}
 	
 	/**
-	 * Sets num of the Box at the given xy-coordinate. 
-	 * @param x x-coordinate
-	 * @param y y-coordinate
-	 * @param num the new num value of the Box
+	 * Sets number of the Box at the given xy-coordinate. 
+	 * @param x x-coordinate.
+	 * @param y y-coordinate.
+	 * @param num the new number value of the Box.
 	 */
 	public void setNum(int x, int y, int num) {
 		setNum(x + y * 3, num);
 	}
 	
 	/**
-	 * Removes the num from avalNum from the Box at
-	 * the given index.
-	 * (count from left to right and up to down)
-	 * @param index index of the box
-	 * @param num the num value to be removed
+	 * Removes the number from avalNum at the Box with the given index.
+	 * @param index index of the box.
+	 * @param num the number value to be removed.
 	 */
 	public void remAvalNum(int index, int num) {
 		this.boxs[index].remAvalNum(num);
 	}
 	
 	/**
-	 * Removes the num from avalNum from the Box at
-	 * the given xy-coordinate.
-	 * @param x x-coordinate
-	 * @param y y-coordinate
-	 * @param num the num value to be removed
+	 * Removes the number from avalNum from the Box at the given xy-coordinate.
+	 * @param x x-coordinate.
+	 * @param y y-coordinate.
+	 * @param num the number value to be removed.
 	 */
+	
 	public void remAvalNum(int x, int y, int num) {
 		remAvalNum(x + y * 3, num);
 	}
 	
 	/**
-	 * Returns the avalNum from the Box at
-	 * the given index.
-	 * (count from left to right and up to down)
-	 * @param index index of the box
-	 * @return ArrayList<Integer>
+	 * Returns the avalNum from the Box at the given index.
+	 * @param index index of the box.
+	 * @return an array list of available numbers.
 	 */
 	public ArrayList<Integer> getAvalNum(int index) {
 		return this.boxs[index].getAvalNum();
 	}
 	
 	/**
-	 * Returns the avalNum from the Box at
-	 * the given xy-coordinate. 
-	 * @param x x-coordinate
-	 * @param y y-coordinate
-	 * @return ArrayList<Integer>
+	 * Returns the avalNum from the Box at the given xy-coordinate. 
+	 * @param x x-coordinate.
+	 * @param y y-coordinate.
+	 * @return an array list of available numbers.
 	 */
 	public ArrayList<Integer> getAvalNum(int x, int y) {
 		return getAvalNum(x + y * 3);
 	}
 	
 	/**
-	 * Sets userNum with the given num for the Box 
-	 * at the given index. 
-	 * (count from left to right up to down)
-	 * @param index index of the box
-	 * @param num the new num value for Box
+	 * Sets userNum with a number for a Box at the given index. 
+	 * @param index index of the box.
+	 * @param num the new num value for Box.
 	 */
+	
 	public void setUserNum(int index, int num) {
 		this.boxs[index].setUserNum(num);
 	}
 	
 	/**
-	 * Sets the userNum with the given num for the Box
-	 * at the given index.  
-	 * @param x x-coordinate
-	 * @param y y-coordinate
-	 * @param num the new num value for Box
+	 * Sets the userNum with a number for the Box at the given index.  
+	 * @param x x-coordinate.
+	 * @param y y-coordinate.
+	 * @param num the new num value for Box.
 	 */
+	
 	public void setUserNum(int x, int y, int num) {
 		setUserNum(x + y * 3, num);
 	}
 	
 	/**
-	 * Returns the userNum from the Box at
-	 * the given index 
-	 * (count from left to right and up to down)
-	 * @param index index of the Box
-	 * @return ArrayList<Integer>
+	 * Retrieves the userNum from the Box at the given index. 
+	 * @param index index of the Box.
+	 * @return array list of numbers from inputed by the user.
 	 */
+	
 	public ArrayList<Integer> getUserNum(int index){
 		return this.boxs[index].getUserNum();
 	}
 	
 	/**
-	 * Returns the userNum from the Box at
-	 * the given xy-coordinate.
-	 * @param x x-coordinate
-	 * @param y y-coordinate
-	 * @return ArrayList<Integer>
+	 * Returns the userNum from the Box at the given xy-coordinate.
+	 * @param x x-coordinate.
+	 * @param y y-coordinate.
+	 * @return array list of numbers from inputed by the user.
 	 */
+	
 	public ArrayList<Integer> getUserNum(int x, int y){
 		return getUserNum(x + y * 3);
 	}
--- src/SudokuGame/Save.java
+++ src/SudokuGame/Save.java
@@ -1,19 +1,34 @@
 package SudokuGame;
 
 import java.io.*;
+/**
+ * The Save class is responsible for serilialsing and saving a board object to a ser file so the player can load previous game states. 
+ * The class is used in view when a user selects the save option.
+ */
 
-import javax.swing.JFileChooser;
 public class Save {
 private Board board;
+/**
+ * This function sets the current Save objects board.
+ * The most recent board set will be saved to a ser file and can be loaded again by a user.
+ * @param board a board object to be written and stored to a file.
+ */
 public void setBoard(Board board) {
 	this.board=board;
 	System.out.println("Board set");
-
 }
+/**
+ * Retrieves the board set for the current Save object.
+ * @return a board object.
+ */
 public Board getBoard() {
 	return this.board;
 }
-
+/**
+ * This function is responsible for saving board to a file. 
+ * The function creates a ser file, serializes it and writes a board object to the file using an ObjectOutputStream.
+ * @return is 0 if the operation was successful or 1 if an error occurred.
+ */
 public int saveFile(){
 	try {
 		FileOutputStream file = new FileOutputStream("save.ser");
